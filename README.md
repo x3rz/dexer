09993da53b48b83107a40921c5f6c3f31267489e
x3rz <cyberlix66@gmail.com>
2020-01-24 12:37:26 -0500

    update

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # dexer
+I use this script to install all the packages and tools that I require for CTFs on one go 
+
+I am trying to make it better and convenient
+
+### Usage
+
+ 1. `git clone https://github.com/x3rz/dexer.git ` 
+ 2. `cd dexter`
+ 3. `chmod +x install.sh`
+ 4. `./install.sh`
+
+
---
+++ install.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+if [ $UID != 0 ]
+then
+	echo "Your are not root"
+	exit
+fi
+sudo apt update
+sudo apt install -y git
+sleep 1
+sudo apt-get install -y sqlitebrowser
+sleep 1
+echo "Installing Sublime Text.."
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install -y apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sleep 1
+sudo apt-get install -y sublime-text
+
+echo "Installing Terminator:" 
+sudo apt-get install -y terminator
+
+echo "Installing taskwarrior for todo lists.."
+sudo apt-get install -y taskwarrior
+
+echo "Installing guake.."
+sudo apt-get install -y guake
+
+echo "Installing nmap:"
+sudo apt-get install -y nmap
+sleep 1
+
+echo "Installing exiftool:"
+sudo apt-get install -y exiftool
+
+echo "Installing sqlitebrowser"
+sudo apt-get install -y sqlitebrowser
+
+sleep 1
+
+echo "Installing Binwalk"
+sudo apt-get install -y binwalk
+sleep 1
+
+echo "Installing Tesseract and Foremost.."
+sudo apt install -y tesseract foremost
+
+echo "Installing ROT11"
+sudo apt install -y bsdgames
+
+echo "Installing hexedit.."
+sudo apt install -y hexedit
+
+echo "Installing zbarimg..."
+sudo apt install -y zbar-tools
+
+sleep 2
+echo "Installing qrencode..."
+sudo apt install -y qrencode
+sleep 2
+echo "Installing pdfcrack..."
+sudo apt install -y pdfcrack
+
+echo "Downloading stegsolve.jar..."
+wget "http://www.caesum.com/handbook/Stegsolve.jar" -O "stegsolve.jar"
+chmod +x "stegsolve.jar"
+
+
+echo "Installing fcrackzip..."
+sudo apt install -y fcrackzip
+
+echo "Installing unrar..."
+sudo apt install -y unrar
+
+echo "Installing steghide..."
+sudo apt install -y steghide
+
+echo "Installing ffmpeg..."
+sudo apt install -y ffmpeg
+
+echo  "Installing scapy"
+sudo apt-get install scapy
+
+echo "Installing tcpflow"
+sudo apt-get install -y tcpflow
+
+
