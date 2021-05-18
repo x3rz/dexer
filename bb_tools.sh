e72734d970020e3bc5b121e6d4bc199b4ff60424
x3rz <cyberlix99@gmail.com>
2021-05-18 15:25:54 +0530

    update

---
+++ bb_tools.sh
@@ -0,0 +1,6 @@
+sudo pacman -S go 
+go get github.com/Emoe/kxss
+go get -u github.com/tomnomnom/assetfinder
+go get github.com/tomnomnom/waybackurls
+GO111MODULE=on go get -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
+# sudo pacman -S nuclei nuclei-templates
